d8a1c0139238ab8d8bdbde2c21da1b977f0ffa89
Lluisarull <lluisa.rull@hotmail.com>
2022-11-16 17:03:20 +0100

    Uploading Data Processing Functions

---
+++ Library_Hw5/src/EinesDeJardiner/Data_Processing.py
@@ -0,0 +1,13 @@
+#DATA PROCESSING
+def clearNA_from_column(df, list_of_columns):
+    # Remove those rows that contain NaN values in the columns: age, gender, ethnicity
+    return df.dropna(subset=list_of_columns, how="any")
+
+
+def fillNA(df, list_of_columns):
+    # Fill NaN with the mean value of the column in the columns: height, weight.
+    for column in list_of_columns:
+        mean = df[column].mean()
+        df[column] = df[column].fillna(mean)
+    return df
+    
